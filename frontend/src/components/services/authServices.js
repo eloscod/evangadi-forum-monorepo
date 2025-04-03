64cd0f4aafe5aa7cfab5742f9a341429f7a7ca3e
Osama Mohammed Hussen <osydosam@gmail.com>
2025-04-03 06:24:42 +0300

    backend edited

--- frontend/src/components/services/authServices.js
+++ frontend/src/components/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 const authAxios = axios.create({
   baseURL: API_URL,
